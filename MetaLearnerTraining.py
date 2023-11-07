e83c78f58099e7258f10b07c90878539c792ca43
aymenhk1 <150078974+aymenhk1@users.noreply.github.com>
2023-11-07 08:24:14 +0100

    MetaLearnerTraining.py

--- MetaLearnerTraining.py
+++ MetaLearnerTraining.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov  6 16:50:49 2023
+Created on Tue Nov  7 08:07:30 2023
 
 @author: Aymen
 """
@@ -28,6 +28,8 @@ from keras import callbacks
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger
 from keras.models import Sequential
  
+print('Welcome MetaLearner Training!')
+
 # load LSTM models from file
 def load_all_models(n_models):
  all_models = list()
@@ -74,48 +76,155 @@ def stacked_prediction(members, model, inputX):
  yhat = model.predict(stackedX)
  return yhat
  
-traindata = pd.read_csv('kddtrain.csv', header=None)
-testdata = pd.read_csv('kddtest.csv', header=None)
+#Load data
+df = pd.read_csv("KDDTrain+.txt")
+test_df = pd.read_csv("KDDTest+.txt")
 
 
-X = traindata.iloc[:,1:42]
-Y = traindata.iloc[:,0]
-C = testdata.iloc[:,0]
-T = testdata.iloc[:,1:42]
+# add the column labels
+columns = (['duration'
+,'protocol_type'
+,'service'
+,'flag'
+,'src_bytes'
+,'dst_bytes'
+,'land'
+,'wrong_fragment'
+,'urgent'
+,'hot'
+,'num_failed_logins'
+,'logged_in'
+,'num_compromised'
+,'root_shell'
+,'su_attempted'
+,'num_root'
+,'num_file_creations'
+,'num_shells'
+,'num_access_files'
+,'num_outbound_cmds'
+,'is_host_login'
+,'is_guest_login'
+,'count'
+,'srv_count'
+,'serror_rate'
+,'srv_serror_rate'
+,'rerror_rate'
+,'srv_rerror_rate'
+,'same_srv_rate'
+,'diff_srv_rate'
+,'srv_diff_host_rate'
+,'dst_host_count'
+,'dst_host_srv_count'
+,'dst_host_same_srv_rate'
+,'dst_host_diff_srv_rate'
+,'dst_host_same_src_port_rate'
+,'dst_host_srv_diff_host_rate'
+,'dst_host_serror_rate'
+,'dst_host_srv_serror_rate'
+,'dst_host_rerror_rate'
+,'dst_host_srv_rerror_rate'
+,'attack'
+,'level'])
 
-scaler = Normalizer().fit(X)
-trainX = scaler.transform(X)
+df.columns = columns
+test_df.columns = columns
+
+# sanity check
+df.head()
+
+# lists to hold our attack classifications
+dos_attacks = ['apache2','back','land','neptune','mailbomb','pod','processtable','smurf','teardrop','udpstorm','worm']
+probe_attacks = ['ipsweep','mscan','nmap','portsweep','saint','satan']
+privilege_attacks = ['buffer_overflow','loadmdoule','perl','ps','rootkit','sqlattack','xterm']
+access_attacks = ['ftp_write','guess_passwd','http_tunnel','imap','multihop','named','phf','sendmail','snmpgetattack','snmpguess','spy','warezclient','warezmaster','xclock','xsnoop']
+
+# we will use these for plotting below
+attack_labels = ['Normal','DoS','Probe','Privilege','Access']
+
+# helper function to pass to data frame mapping
+def map_attack(attack):
+    if attack in dos_attacks:
+        # dos_attacks map to 1
+        attack_type = 1
+    elif attack in probe_attacks:
+        # probe_attacks mapt to 2
+        attack_type = 2
+    elif attack in privilege_attacks:
+        # privilege escalation attacks map to 3
+        attack_type = 3
+    elif attack in access_attacks:
+        # remote access attacks map to 4
+        attack_type = 4
+    else:
+        # normal maps to 0
+        attack_type = 0
+        
+    return attack_type
+
+class_names =[0,1,2,3,4]
+
+# map the data and join to the data set
+attack_map = df.attack.apply(map_attack)
+df['attack_map'] = attack_map
+
+test_attack_map = test_df.attack.apply(map_attack)
+test_df['attack_map'] = test_attack_map
+
+# view the result
+df.head()
+
+set(df['attack_map'])
+
+df.head()
+
+
+df['attack'] = df['attack_map']
+test_df['attack'] = test_df['attack_map']
+
+X_kdd = df.drop(['protocol_type',	'service','flag','level','attack_map','attack'],axis=1)
+T_kdd = test_df.drop(['protocol_type',	'service','flag','level','attack_map','attack'],axis=1)
+
+# create our target classifications
+Y_kdd = df['attack']
+C_kdd = test_df['attack']
+
+
+# Pre-process data
+scaler = Normalizer().fit(X_kdd)
+trainX = scaler.transform(X_kdd)
 # summarize transformed data
 np.set_printoptions(precision=3)
 #print(trainX[0:5,:])
 
-scaler = Normalizer().fit(T)
-testT = scaler.transform(T)
+scaler = Normalizer().fit(T_kdd)
+testT = scaler.transform(T_kdd)
 # summarize transformed data
 np.set_printoptions(precision=3)
 #print(testT[0:5,:])
 
 
-y_train1 = np.array(Y)
-y_test1 = np.array(C)
+y_train1 = np.array(Y_kdd)
+y_test1 = np.array(C_kdd)
 
 y_train= to_categorical(y_train1)
 y_test= to_categorical(y_test1)
 
-print(trainX.shape, testT.shape)
 # load all models
-n_members = 5
+n_members = 3
 members = load_all_models(n_members)
 print('Loaded %d models' % len(members))
 
 # evaluate standalone models on test dataset
 for model in members:
- y_test1 = np.array(C)
+ y_test1 = np.array(C_kdd)
  y_test= to_categorical(y_test1)
  _, acc = model.evaluate(testT, y_test, verbose=0)
  print('Model Accuracy: %.3f' % acc)
 # fit stacked model using the ensemble
 model = fit_stacked_model(members, testT, y_test1)
+filename = 'models/model_F.h5'
+model.save(filename)
+print('>Saved %s' % filename)
 # evaluate model on test set
 yhat = stacked_prediction(members, model, testT)
 acc = accuracy_score(y_test1, yhat)
